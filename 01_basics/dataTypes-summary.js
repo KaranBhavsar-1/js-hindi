6e5a65fde39f61665ea8b3b82a9e34fbafce17ae
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2025-10-27 14:23:51 +0000

    Data Types Summary - 2

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -1,34 +1,64 @@
-// primitve datatypes 
-//Types:- String, Number, Boolean, Null, Undefined, Symbol ,BigInt (too big number)
-const name = "Karan"  //String
-const rollNo = 3      // Number
-const isPresent = true//Boolean
-const outsideTemp = null //Object
-let userMain;            // Undefined
-const id = Symbol('123') //Symbol
-const idNext = Symbol('123')  //Symbol
-console.log(id != idNext)     // Not equal because of Symbol datatype
-const bigNumber = 2134678902345678n // now the datatype is BigInt
-console.log(typeof bigNumber)
+// // primitve datatypes 
+// //Types:- String, Number, Boolean, Null, Undefined, Symbol ,BigInt (too big number)
+// const name = "Karan"  //String
+// const rollNo = 3      // Number
+// const isPresent = true//Boolean
+// const outsideTemp = null //Object
+// let userMain;            // Undefined
+// const id = Symbol('123') //Symbol
+// const idNext = Symbol('123')  //Symbol
+// console.log(id != idNext)     // Not equal because of Symbol datatype
+// const bigNumber = 2134678902345678n // now the datatype is BigInt
+// console.log(typeof bigNumber)
 
 
-// non-primirive tyeps OR reference types
-// Array, Object , Functions
+// // non-primirive tyeps OR reference types
+// // Array, Object , Functions
 
-const heros = ["Shaktiman", "Sambar" , "Idli", "dhokla"];   //Array
-console.log(typeof heros)
-//Object
-let myObj = {
-    name: "Karan",
-    age: 22,
-    sex: "chalega"
+// const heros = ["Shaktiman", "Sambar" , "Idli", "dhokla"];   //Array
+// console.log(typeof heros)
+// //Object
+// let myObj = {
+//     name: "Karan",
+//     age: 22,
+//     sex: "chalega"
+// }
+// console.log(typeof myObj)
+// console.log(myObj)
+
+// const myFun = function(){
+//     console.log("Hello World!!!");
+
+// }
+// myFun()
+// console.log(typeof myFun)
+
+
+// *************************Memories **********************
+//2 Types:- Stack (Primitive), changes Copy
+// Heap (non-premitive //reference type), changes Original
+
+//Stack
+// let myName  = "Karan Bhavsar"
+// let myAnotherName = myName
+// console.log(myName)
+// console.log(myAnotherName)
+// myAnotherName = "Donkey"
+
+// console.log(myName)
+// console.log(myAnotherName)
+
+//Heap
+
+let myData = {
+    name: "Karan Bhavsar",
+    id: 21
 }
-console.log(typeof myObj)
-console.log(myObj)
 
-const myFun = function(){
-    console.log("Hello World!!!");
+let anotherData = myData
+console.log(myData)
+console.log(anotherData)
+anotherData.id = 20
 
-}
-myFun()
-console.log(typeof myFun)
+console.log(myData)
+console.log(anotherData)
