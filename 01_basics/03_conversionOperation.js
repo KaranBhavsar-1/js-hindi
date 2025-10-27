77d54fd11ddf172a842f03fc0be77dad111313eb
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2025-10-27 13:27:51 +0000

    Data type conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let age = "20asd"
+// let score = "33"
+// let name = null
+// let place = false
+// // for string that is all numbers to number
+// console.log(typeof score)
+// score = Number(score)
+// console.log(typeof score)
+
+// //for string that is text into number
+// console.log(typeof age)
+// age = Number(age)
+// console.log(typeof age)
+// console.log(age)
+
+// // for null to number
+// console.log(typeof name)
+// name = Number(name)
+// console.log(typeof name)
+// console.log(name)
+
+// // for boolean to number
+// // if true = 1, if false = 0
+// console.log(typeof place)
+// place = Number(place)
+// console.log(typeof place)
+// console.log(place)
+
+//from number to boolean
+let boolval = 0
+console.log(typeof boolval)
+boolval = Boolean(boolval)
+console.log(typeof boolval)
+console.log(boolval)
