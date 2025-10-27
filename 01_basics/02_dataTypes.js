7925f5798f18fa3ac6d64ec844544a748828c50e
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2025-10-27 03:44:05 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  //Meaning to use the newer version only for the below code
+
+// alert(3+3) //we are using node.js , not browser
+
+//get info on "mdn" or "tc39"(official for node js)
+// example:-  for alert "alertmdn" on browser.
+
+
+/*
+//datatypes = 
+numbers
+bigint
+"string"
+boolean (true/fales)
+null (stand alone value) an object = "some value but empty"
+undefined  = "no value at all"
+symbol = unique
+*/
+
+let name
+console.log(typeof name);
