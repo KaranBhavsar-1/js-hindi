a724ee09dba177685a1664029753af1069c6bc56
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2025-10-31 12:54:59 +0000

    Funtion 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -4,7 +4,7 @@ function sayMyName(){
     console.log("Donkey")
 }
 //Call Function
-sayMyName()
+// sayMyName()
 
 // function add2Numbers(a,b){      //Parameters
 //     console.log(`the addition of number ${a} and ${b} is ${a+b}`)
@@ -25,4 +25,39 @@ function loginUserMessage(userName = "Doneky"){
     return `${userName} just logger in`
 }
 let message = loginUserMessage("Karan")
-console.log(message)
+// console.log(message)
+
+
+function calculateCartPrice(val1,val2,...num1){               //take mutlitple value
+    return num1
+}
+
+// console.log(calculateCartPrice(200,300,400,500,600,700))
+
+
+//take object as input (Parameters) 
+const user = {              //Object
+    username: "Karan",
+    price: 199
+}
+
+//Function
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and Price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "Donkey",
+//     price: 122
+// })
+
+//Array as input
+
+const myArr = [0,1,2,3,4]
+
+function returnArrayValues(Arr){
+    return `the 2nd value in Array is ${Arr[1]}`
+}
+console.log(`Array  = [${myArr}]`)
+console.log(returnArrayValues(myArr))
