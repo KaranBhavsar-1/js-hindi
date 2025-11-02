ac2d6a76976a69ee35a1fd9c1c0c8c1077ed4491
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2025-11-02 13:39:44 +0000

    while and Do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+//****************while and Do-while Loop */
+
+//********WHILE LOOP
+// i=0
+// while (i<=2) {
+//     // console.log("the loop is on " + i)
+//     i++
+// }
+
+// //for ARRAY
+
+// myArr = ["bird","bitch","asd","jail","seal","kill","bole"]
+// i=0
+// while (i<myArr.length) {
+//     console.log(`[${myArr[i]}]`)
+//     i++
+// }
+
+//**********DO-WHILE
+score = 110
+do {
+    console.log(`the score is [${score}]`)
+    score++
+} while (score <=10)
+
+// check the condition ater and does the  job 1st
