b896e942962b22e88bec945660ee78c5d06d8bfd
KaranBhavsar-1 <karanbhavsar0001@gmail.com>
2025-11-02 13:41:05 +0000

    ForEach Loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,37 @@
+const coding = ["js", "Python","Java", "Cobra"]
+
+
+// normal for ecah function 
+// coding.forEach( function (item) {
+//     console.log(item)
+// })
+
+//Arrow Function
+// coding.forEach( (item)=> {
+//     console.log(item)
+// })
+
+//Print me
+// just prints whatever the thing is
+// function printMe(item){console.log(item)}
+// coding.forEach(printMe)
+
+// coding.forEach( (item , index, arr) => {
+//     console.log(item,index,arr)
+// })
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    }
+]
+ myCoding.forEach((item) => {console.log(item.languageName)})
